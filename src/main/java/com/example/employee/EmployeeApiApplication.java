b8f703eaa3792a28afe5c0d196e1e93c383d014b
Nasiroddin Qatib <nasirkhatib01@gmail.com>
2026-03-06 11:56:23 -0800

    Update EmployeeApiApplication.java

--- src/main/java/com/example/employee/EmployeeApiApplication.java
+++ src/main/java/com/example/employee/EmployeeApiApplication.java
@@ -2,12 +2,18 @@ package com.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeApiApplication {
+public class EmployeeApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApiApplication.class, args);
     }
-
 }
